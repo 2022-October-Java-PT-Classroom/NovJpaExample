231dd11fbd01e549b25c42b2410f95380c3c4a66
jordanCCIT <jordanadposting@gmail.com>
2022-11-05 15:38:36 -0400

    initcommit

---
+++ src/test/java/com/example/NovIntroToJpaDemo/NovIntroToJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NovIntroToJpaDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovIntroToJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
